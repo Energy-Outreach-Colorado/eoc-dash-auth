ff666a39a24b74aff3bd8f0a1511d067168880e2
Toby Penner <tobypenner01@gmail.com>
2024-02-03 12:20:10 -0700

    Update auth check to check dashboard list (untested)

--- eoc_dash_auth/__init__.py
+++ eoc_dash_auth/__init__.py
@@ -1,5 +1,6 @@
 import requests
 import werkzeug
+from dash import Dash
 from dash_auth.auth import Auth  # type: ignore[import-untyped]
 from flask import Response, redirect, request
 
@@ -8,15 +9,28 @@ BASE_API_URL = "http://localhost:8000"
 
 
 class EOCAuth(Auth):
+    def __init__(self, app: Dash, path: str, **obsolete):
+        super().__init__(app, **obsolete)
+
+        if len(path) < 1:
+            raise ValueError("EOCAuth path must be at least one character")
+        if path[0] == "/":
+            raise ValueError("Ensure path doesn't contain a leading slash")
+        self.path = path
+
     def is_authorized(self) -> bool:
         auth_cookie = request.cookies.get("fastapiusersauth", None)
         if auth_cookie is None:
             return False
 
         response = requests.get(
-            f"{BASE_API_URL}/users/me", cookies={"fastapiusersauth": auth_cookie}
+            f"{BASE_API_URL}/dashboards/", cookies={"fastapiusersauth": auth_cookie}
         )
-        return response.status_code == 200
+        if not response.ok:
+            return False
+
+        dashboard_paths = [dashboard["path"] for dashboard in response.json()]
+        return self.path in dashboard_paths
 
     def login_request(self) -> werkzeug.Response:
         return redirect(BASE_FRONTEND_URL, code=307)
